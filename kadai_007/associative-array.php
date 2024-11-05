f36eabb3974428be6488e403f470c7a6934f4177
azuoru <music.thecats@gmail.com>
2024-11-05 15:58:16 +0900

    kadai_007を提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP課題007</title>
+ </head>
+
+ <body>
+    <p>
+        <?php
+        // 連想配列
+        $vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // 出力
+        print_r($vegetable_data);
+        ?>
+    </p>
+</body>
+
+</html>
