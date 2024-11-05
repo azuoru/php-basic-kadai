3145f1282a3da6f8740e55237c355426c0446d18
azuoru <music.thecats@gmail.com>
2024-11-05 14:27:35 +0900

    再提出

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -21,8 +21,14 @@
     $score9 = 30;
     $score10 = 60;
 
-    // 計算式
-    echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
+    // 合計
+    $full = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+    
+    // 平均点
+    $average = $full / 10;
+
+    // 出力
+    echo $average;
     ?>
     </p>
 </body>
